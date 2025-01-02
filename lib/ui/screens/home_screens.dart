f092b34114bedc0eb50b325f502bc3d87b4f51c0
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2025-01-02 21:47:49 +0600

    counterApp_Advance Get_x

--- lib/ui/screens/home_screens.dart
+++ lib/ui/screens/home_screens.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
