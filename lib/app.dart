e7cade9933fddbc3d9b900f9f192f47e424aa4e2
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2025-01-02 21:08:54 +0600

    counterApp_Get_X

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:ostad_batch_07/ui/screens/home_screens.dart';
-
 
 class CounterApp extends StatefulWidget {
   const CounterApp({super.key});
--- lib/ui/screens/home_screens.dart
+++ lib/ui/screens/home_screens.dart
@@ -1,3 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  RxInt _counter = 0.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Obx((){
+          return   Text(
+        "Taka:$_counter",
+        style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: Colors.greenAccent,
+        ),
+        );}
+
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+      /*  onPressed: () {
+            _counter.value++;
+        },*/
+        onPressed:uiUpdate ,
+        child: const Icon(Icons.add),
+      ),
+    );
+
+  }
+  void uiUpdate(){
+    _counter++;
+  }
+}
