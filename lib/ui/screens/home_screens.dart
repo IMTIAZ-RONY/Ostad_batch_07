9a392c4805f0c989d4a968baacab4fdb2052b4cf
IMTIAZ SHAMIM RONY <117357772+IMTIAZ-RONY@users.noreply.github.com>
2025-01-02 21:46:51 +0600

    counterApp_Advance Get_x

--- lib/ui/screens/home_screens.dart
+++ lib/ui/screens/home_screens.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
@@ -11,35 +11,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  RxInt _counter = 0.obs;
+CounterController counterController= CounterController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Obx((){
-          return   Text(
-        "Taka:$_counter",
-        style: const TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-        color: Colors.greenAccent,
+        child: GetBuilder(
+          init:counterController ,
+            builder:(_){
+              return   Text(
+                "Taka:$counterController.counter",
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.greenAccent,
+                ),
+              );
+            } )
         ),
-        );}
-
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-      /*  onPressed: () {
-            _counter.value++;
-        },*/
-        onPressed:uiUpdate ,
+
+        onPressed:counterController.increment ,
         child: const Icon(Icons.add),
       ),
-    );
+      );
+
+
 
   }
-  void uiUpdate(){
-    _counter++; ///l
+
+}
+class CounterController extends GetxController{
+  int _counter = 0;
+  get counter=>_counter;
+  void increment(){
+    _counter++;
+    update();
   }
+
 }
